2bc42fd95ba9d41dbd686fafc00a0e2a7c09df84
arpanwar <arpanwar@iu.edu>
2021-10-29 21:56:11 -0400

    Added update and delete

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -4,7 +4,8 @@ const SomeApp = {
       return {
         person: {},
         books: [],
-        booksForm:{}
+        booksForm:{},
+        selectedbook: null
 
       }
     },
@@ -53,6 +54,61 @@ const SomeApp = {
                 // reset the form
                 this.booksForm = {};
               });
+              
+          },
+          postEdit(evt) {
+            this.booksForm.id = this.selectedbook.id;      
+            
+            console.log("Editing!", this.booksForm);
+    
+            fetch('api/books/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.booksForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+          },
+          postDeleteBook(book) {  
+            if ( !confirm("Are you sure you want to delete the book from " + book.title + "?") ) {
+                return;
+            }  
+            
+            console.log("Delete!", book);
+    
+            fetch('api/books/delete.php', {
+                method:'POST',
+                body: JSON.stringify(book),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.handleResetEdit();
+              });
+          },
+          handleEditBook(book) {
+              this.selectedbook = book;
+              this.booksForm = Object.assign({}, this.selectedbook);
+          },
+          handleResetEdit() {
+              this.selectedbook = null;
+              this.booksForm = {};
           }
     },
     created() {
